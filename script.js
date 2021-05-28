207f99886c6ea41a9bc561955d4f9c008ccc1979
박유경 <pyk0514@kumoh.ac.kr>
2021-05-29 04:59:36 +0900

    틀 초안

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+// SideBar
+function openNav() {
+    document.getElementById("menuBar").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("menuBar").style.width = "0";
+}
